150ec90ad4d497569d13e2fc4b7a298b9b681a1b
Errindel <adriana.olszak@gmail.com>
2017-06-24 21:48:37 +0200

    Simple Node Notes app

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("app.js started");
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes');
+
+
+var command = process.argv[2];
+console.log("command: ", command);
+
+
+if (command === 'add'){
+
+}
---
+++ notes.js
@@ -0,0 +1,8 @@
+console.log('notes.js started');
+
+module.exports = {
+  addNote: function(){
+    console.log('addNote');
+    return 'New Note';
+  }
+};
